d403d6816455474a10bffabcc14ab473c726c320
Lilian PETIT <zeta.zero.76@gmail.com>
2025-12-03 20:53:20 +0100

    ptite modif des explications rien de fou

--- MAIN_F_AI/NEURON NETWORK/TEST.py
+++ MAIN_F_AI/NEURON NETWORK/TEST.py
@@ -5,17 +5,17 @@ from C_Network import train
 
 # Exemple d'entrées pour entraîner le réseau de neurones :
 X = np.array([
-    [0.6, -1.5], #array 0
-    [1.0, 0.5] #array 1
+    [0.6, -1.5],   #array 0
+    [1.0, 0.5]     #array 1
     ])
 
 y = np.array([1, 0])  # Valeurs cibles associées aux arrays d'entrée (entre 0 et 1 pour sigmoïde)
 
-# Création du réseau de neurones avec 3 couches, chaque neurone represente une entree pour la couche suivante
+# Création du réseau de neurones avec 3 couches, chaque neurone a une sortie et donc une entree pour la couche suivante
 nn = NeuralNetwork([
-    Layer(n_inputs=2, n_neurons=3),  # Couche d'entrée avec 3 neurones et 2 entrées
-    Layer(n_inputs=3, n_neurons=2),  # Couche cachée avec 2 neurones et 3 entrées
-    Layer(n_inputs=2, n_neurons=1)   # Couche de sortie avec 1 neurone et 2 entrées
+    Layer(n_inputs=2, n_neurons=3),  # Couche d'entrée avec 2 entrées et 3 neurones
+    Layer(n_inputs=3, n_neurons=2),  # Couche cachée avec 3 entrées et 2 neurones
+    Layer(n_inputs=2, n_neurons=1)   # Couche de sortie avec 2 entrées et 1 neurone
 ])
 
 train(nn, X, y, lr=0.5, epochs=10000) # Entraînement du réseau de neurones
--- MAIN_F_AI/SINGLE NEURON/train.py
+++ MAIN_F_AI/SINGLE NEURON/train.py
@@ -6,14 +6,13 @@ def train(neuron, X, y, lr=0.1, epochs=1000): #epochs = nombre d'itérations d'e
         for inputs, target in zip(X, y):
             # on test la prédiction du neurone :
             z = np.dot(neuron.weights, inputs) + neuron.bias #calcul de z
-            pred = neuron.activation(z) #application de la fonction d'activation (sigmoïde, ReLU, linéaire)
-            
+            pred = neuron.activation(z) # application de la fonction d'activation (sigmoïde uniquement jsp pk ca marche pas avec les autres mais osef)
             # calcul de l'erreur :
-            error = pred - target #fonction de perte (MSE). On utilise pas le Log Loss.
+            error = pred - target # différence entre la prédiction et la cible y
             
-            # gradient (fonction d'activation dérivée) :
+            # gradient (fonction sigmoide dérivée) :
             grad_pred = pred * (1 - pred)
             
             # update du neurone :
-            neuron.weights -= lr * error * grad_pred * inputs #mise à jour des poids (plus d'aleatoire)
-            neuron.bias -= lr * error * grad_pred #mise à jour du biais
+            neuron.weights -= lr * error * grad_pred * inputs # mise à jour des poids (plus d'aleatoire)
+            neuron.bias -= lr * error * grad_pred # mise à jour du biais
